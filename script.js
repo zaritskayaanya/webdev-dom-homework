fa42603b5c77b7cea7a62b348b3d2ff9b0d12c61
ShowTimeQQ <cebep.pnz@yandex.ru>
2024-09-20 20:51:06 +0300

    hw-02_V1

---
+++ script.js
@@ -0,0 +1,68 @@
+let age = 10;
+alert(age);
+let up = 20;
+alert(up);
+let date = ('Дата выхода Iphone1 20.06.2007');
+alert(date);
+let namejs = ('создатель js Brendan Eich');
+alert(namejs);
+
+let addition = 10;
+let additionup = 2;
+let result = addition + additionup ;
+alert(result);
+
+let difference = 10;
+let differencebot = 2;
+let result2 = difference - differencebot;
+alert(result2);
+
+let divide = 10;
+let dividebot = 2;
+let result3 = divide / dividebot;
+alert(result3);
+
+let i = 2**5;
+alert(i);
+
+let a = 9;
+let b = 2;
+let x = 9 / 2;
+alert(x);
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num++;
+num--;
+alert(num);
+
+let age1 = Number(prompt('Сколько Вам лет?'));
+alert(age1);
+
+const user = {
+    name: 'Mickey',
+    age: 37 ,
+    Boolean: "isAdmin"
+ };
+ 
+ let info = prompt("Что вы хотите узнать о пользователе?", "name");
+ 
+console.log(user[info]); 
+
+
+let privet = 'Привет,';
+let name3 =String(prompt('Как Вас зовут?'));
+let sign = '!';
+result4 = privet + name3 + sign;
+alert( result4 );
+
+let info10 = prompt('Как твоё имя ?');
+alert(`Зaдание 10: Привет, ${info10}!`);
+
+
+
+
+
